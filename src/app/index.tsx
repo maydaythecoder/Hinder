d0de5840049df6f53d2c4361b8ae67f997dcb0e4
Muhyadin <Muhyadin.Mohamed.003@gmail.com>
2026-08-02 22:00:34 -0400

    added core docs in index file

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,21 @@
+/*
+the end goal of this MVP is to have swipable cards, functional chats, and an editable userinterface
+post mvp the first push will be for bug fixes and optimizations aswell as docs
+following that ill begin to impliment new ideas or improving the user interface
+and finally thee last push in this loop will be for production and hosting readiness
+
+dev loop:
+  1 mvp
+    2 bug fixes and optimizations/docs
+    3 new ideas and ui improvements
+    4 production and hosting readiness
+
+1 will only be done once and 2,3,and 4 will be iterated over
+the only case which will call for 1 to be reinstated is if a new core feature is being added
+a core feature is defined by the following:
+  - a change that alters the ui/ux in a way that is fundamentally different from the prior state
+  - an addition of a functionality that alters the reward/encouraged behavior of the user
+*/
 import * as Device from 'expo-device';
 import { Platform, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
